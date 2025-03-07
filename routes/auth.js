eea9de4c1ebf606bb65d77805331a23277883cba
ct <978744151@qq.com>
2025-03-07 16:52:12 +0800

    first

---
+++ routes/auth.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const {
+  register,
+  login,
+  getMe,
+  getAllUser,
+  updateDetails,
+  updatePassword,
+  forgotPassword,
+  resetPassword,
+} = require("../controllers/auth.js");
+
+// 路由鉴权
+const { protect } = require("../middleware/auth.js");
+
+router.post("/register", register);
+router.post("/login", login);
+router.get("/me", protect, getMe);
+router.get("/all/user", protect, getAllUser);
+router.put("/updatedetails", protect, updateDetails);
+router.put("/updatepassword", protect, updatePassword);
+router.post("/forgotpassword", forgotPassword);
+router.put("/resetpassword/:resettoken", resetPassword);
+
+module.exports = router;
