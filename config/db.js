ee3aa89f49d929dcb47344f8dea29e638f339266
ct <978744151@qq.com>
2025-03-24 10:09:23 +0800

    serverSelectionTimeoutMS: 20000, // 增加到 20 秒

--- config/db.js
+++ config/db.js
@@ -2,15 +2,16 @@ const mongoose = require('mongoose');
 const dotenv = require("dotenv");
 
 // mongoose.connect('mongodb://admin:你的密码@8.155.53.210:27017/yourdb?authSource=admin');
-const path =  process.env.NODE_ENV === 'production' ? "./config/config.prod.env" : './config/config.dev.env'
+const path = process.env.NODE_ENV === 'production' ? "./config/config.prod.env" : './config/config.dev.env'
 dotenv.config({
   path
 });
 const connectDB = async () => {
   const conn = await mongoose.connect(
     process.env.SERVER_DB, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 20000, // 增加到 20 秒
   });
   mongoose.connection.on("error", function (error) {
     console.log("数据库连接失败：" + error)
